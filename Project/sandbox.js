ab6c3ab080500356ba8e6580e08c40e146ba894c
Neetu Rai <raineetu0070@gmail.com>
2025-07-04 19:09:35 +0545

    chore:added compiler for typescript

---
+++ Project/sandbox.js
@@ -0,0 +1,8 @@
+var character = "neetu";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+  console.log(input);
+});
+//tsc sandbox.ts and it will automatically make sandbox.js
---
+++ Project/sandbox.ts
@@ -0,0 +1,11 @@
+const character = "neetu";
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+inputs.forEach((input) =>{
+    console.log(input)
+})
+
+//tsc sandbox.ts and it will automatically make sandbox.js

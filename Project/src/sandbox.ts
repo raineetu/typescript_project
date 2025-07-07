d200a2be5cba3dace3fb20fc8804dc44adffc73f
Neetu Rai <raineetu0070@gmail.com>
2025-07-07 21:10:35 +0545

    refactor te code

--- Project/src/sandbox.ts
+++ Project/src/sandbox.ts
@@ -1,11 +1,11 @@
 // String Variable
 const character = "neetu";
-console.log(character); 
+console.log(character);
 
 //DOM Selection using querySelectorAll
 // This selects all input elements on the page
 const inputs = document.querySelectorAll("input");
-console.log(inputs); 
+console.log(inputs);
 
 inputs.forEach((input) => {
   console.log(input); // Logs each input element
@@ -15,17 +15,17 @@ inputs.forEach((input) => {
 let numbers: number[] = [1, 2, 3, 4, 5]; // Explicitly typed as number[]
 numbers.push(6); // add at last
 numbers.unshift(5) // add at first
-numbers.splice(2,0,25)// start index, delete count and added value
+numbers.splice(2, 0, 25)// start index, delete count and added value
 
 //  Mixed Array
 let mixed = ["sd", 4, "s"]; // Type: (string | number)[]
-mixed.push(5); 
+mixed.push(5);
 // mixed.push(false);  Error: boolean is not assignable
 console.log(mixed); // ['sd', 4, 's', 5]
 
 // Explicit Type Array
 let fruits: string[] = ["apple", "banana"];
-fruits.push("mango"); 
+fruits.push("mango");
 // fruits.push(5);  Error: number is not assignable to string
 console.log(fruits); // ['apple', 'banana', 'mango']
 
@@ -87,11 +87,11 @@ console.log(user1);
 //function in typescript
 let greetUser: Function;
 
-greetUser = () =>{
+greetUser = () => {
   console.log("hello")
 }
 
-const add =(a:number, b:number, c?:number | string) =>{
-console.log(a+b)
+const add = (a: number, b: number, c?: number | string) => {
+  console.log(a + b)
 }
-add(5,7)
+add(5, 6)
